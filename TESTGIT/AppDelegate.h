0afd2bbedcc64c59ce4088e386dc634ae01dd3bf
mohamedaslam28 <mohamedaslam28@gmail.com>
2018-11-19 15:55:58 +0800

    Initial Commit

---
+++ TESTGIT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TESTGIT
+//
+//  Created by intretech on 2018/11/19.
+//  Copyright © 2018 intretech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TESTGIT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TESTGIT
+//
+//  Created by intretech on 2018/11/19.
+//  Copyright © 2018 intretech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
