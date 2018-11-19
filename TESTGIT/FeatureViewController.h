3e028ac4db6eee195680833f96a6674e92cc6b33
mohamedaslam28 <mohamedaslam28@gmail.com>
2018-11-19 15:58:03 +0800

    Feature added

---
+++ TESTGIT/FeatureViewController.h
@@ -0,0 +1,17 @@
+//
+//  FeatureViewController.h
+//  TESTGIT
+//
+//  Created by intretech on 2018/11/19.
+//  Copyright © 2018 intretech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FeatureViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TESTGIT/FeatureViewController.m
@@ -0,0 +1,32 @@
+//
+//  FeatureViewController.m
+//  TESTGIT
+//
+//  Created by intretech on 2018/11/19.
+//  Copyright © 2018 intretech. All rights reserved.
+//
+
+#import "FeatureViewController.h"
+
+@interface FeatureViewController ()
+
+@end
+
+@implementation FeatureViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
